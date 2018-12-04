70dcc98dbbddca1d566cc012d7dfadc0fb0d6138
elim322 <einer.lim322@gmail.com>
2018-12-04 13:18:27 -0800

    remove unused styles

--- js/App.js
+++ js/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import AppNavigator from "./navigation/RootStackNavigator";
 
 export default class App extends Component {
