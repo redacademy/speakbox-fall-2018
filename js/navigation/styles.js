2ce49cfc101807271b037655ce30cc47f1cdad22
elim322 <einer.lim322@gmail.com>
2018-12-05 14:24:49 -0800

    import png icons into bottom navigation bar and change heights and widths into dimensions

--- js/navigation/styles.js
+++ js/navigation/styles.js
@@ -1,13 +1,21 @@
-import { StyleSheet } from "react-native";
-export default (style = StyleSheet.create({
+import { StyleSheet, Dimensions } from "react-native";
+const styles = StyleSheet.create({
   header: {
     flex: 1,
     borderBottomColor: "white"
   },
   profileIcon: {
-    height: 35,
-    width: 35,
+    width: Dimensions.get("window").width / 8,
+    height: Dimensions.get("window").height / 8,
+    resizeMode: "center",
     alignSelf: "flex-end",
     marginRight: 10
+  },
+  navIcons: {
+    width: Dimensions.get("window").width / 12,
+    height: Dimensions.get("window").height / 12,
+    resizeMode: "center"
   }
-}));
+});
+
+export default styles;
