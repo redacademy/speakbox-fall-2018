6f6a1f421003ab4055b7dd8d9abe00bb2b6222dd
Juett <juettdm@gmail.com>
2018-12-05 15:54:00 -0800

    reminder component

---
+++ js/components/SetAReminder/SetAReminder.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Text, View } from "react-native";
+class SetAReminder extends Component {
+  constructor(){
+    super()
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <View>
+        <Text>SetAReminder</Text>
+      </View>
+    );
+  }
+}
+
+SetAReminder.propTypes = {};
+
+export default SetAReminder;
