5db6252ec593f9c9577d421d62cdefb9195c2e62
julia browne <julia.browne@live.ca>
2018-12-07 09:36:48 -0800

    unlink and remove react-native-svg package (#55)

--- android/app/src/main/java/com/speakbox/MainApplication.java
+++ android/app/src/main/java/com/speakbox/MainApplication.java
@@ -3,7 +3,6 @@ package com.speakbox;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
 import io.realm.react.RealmReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SvgPackage(),
             new RealmReactPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
