76d41b3a51711b068d9394068256666809f5f6e1
julia browne <julia.browne@live.ca>
2018-12-07 09:33:19 -0800

    install prop-types and image-picker packages

--- android/app/src/main/java/com/speakbox/MainApplication.java
+++ android/app/src/main/java/com/speakbox/MainApplication.java
@@ -3,7 +3,7 @@ package com.speakbox;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
+import com.imagepicker.ImagePickerPackage;
 import io.realm.react.RealmReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SvgPackage(),
+            new ImagePickerPackage(),
             new RealmReactPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
