119dacbf07aa40acb4d4a77bfd90fe6913a7fb4e
Julia Browne <julia.browne@live.ca>
2018-12-06 17:02:49 -0800

    add fetch api to be used by all on team

---
+++ js/config/api.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+const getAPI = (path, headerConfig) => {
+  const baseURL = 'https://api.speakbox-staging.ca/api'
+  const defaultHeaderOptions = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+  const headerOptions = {
+    ...headerConfig,
+    headers: { ...headerConfig.headers, ...defaultHeaderOptions }
+  }
+  return fetch(baseURL + path, headerOptions)
+    .then(res => res.json())
+    .then(res => res)
+    .catch(e => e)
+}
+
+export default getAPI
