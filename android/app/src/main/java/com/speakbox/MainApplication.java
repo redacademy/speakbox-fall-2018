3fb5ceff5261d49c679f7ff415a5131644dec1c6
julia browne <julia.browne@live.ca>
2018-12-10 14:34:38 -0800

    install notifications and onboarding-swiper packages (#81)
    * install moment package
    
    * install react-native-chart-kit package
    
    * install notifications and onboarding packages

--- android/app/src/main/java/com/speakbox/MainApplication.java
+++ android/app/src/main/java/com/speakbox/MainApplication.java
@@ -3,6 +3,7 @@ package com.speakbox;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
 import io.realm.react.RealmReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNNotificationsPackage(),
             new RealmReactPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
