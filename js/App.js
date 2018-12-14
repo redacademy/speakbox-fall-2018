43157475904d4cd3a9b8038eda174697bc1be239
ajaguasin <ajaguasin@gmail.com>
2018-12-14 10:12:51 -0800

    Resolve merge conflicts

--- js/App.js
+++ js/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import AppNavigator from "./navigation/RootStackNavigator";
-import UserLoginContainer from './screens/UserLogin/UserLoginContainer'
+import UserLoginContainer from "./screens/UserLogin/UserLoginContainer";
 
 export default class App extends Component {
   render() {
-    return <UserLoginContainer />;
+    return <AppNavigator />;
   }
 }
