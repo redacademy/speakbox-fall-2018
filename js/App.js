3a5260975a8770f3d5f389d43a89228214b61a94
Julia Browne <julia.browne@live.ca>
2018-12-18 11:24:54 -0800

    clean up app.js file

--- js/App.js
+++ js/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import AppNavigator from './navigation/RootStackNavigator'
-// import UserSigninContainer from './screens/UserSignin/UserSigninContainer'
-// import { AuthStack } from './navigation/NavigationLayout'
 
 export default class App extends Component {
   render() {
